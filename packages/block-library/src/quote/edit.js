ada58283e8a26bf23ba8d0e6c4f9c081212b04f0
Andrés <andres.maneiro@automattic.com>
2020-10-07 10:57:59 +0200

    Format

--- packages/block-library/src/quote/edit.js
+++ packages/block-library/src/quote/edit.js
@@ -76,7 +76,9 @@ export default function QuoteEdit( {
 							: InnerBlocks.ButtonBlockAppender
 					}
 				/>
-				{ ( ! RichText.isEmpty( citation ) || isSelected || isAncestorOfSelectedBlock ) && (
+				{ ( ! RichText.isEmpty( citation ) ||
+					isSelected ||
+					isAncestorOfSelectedBlock ) && (
 					<RichText
 						identifier="citation"
 						value={ citation }
